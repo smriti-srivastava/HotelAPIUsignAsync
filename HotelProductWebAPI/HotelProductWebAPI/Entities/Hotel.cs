83a86f594e17669a905fd6da949da04e563a578c
Smriti Srivastava <ssrivastava@tavisca.com>
2018-09-22 22:07:44 +0530

    Hotel Api with Async Programming

---
+++ HotelProductWebAPI/HotelProductWebAPI/Entities/Hotel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelProductWebAPI.Entities
+{
+    public class Hotel
+    {
+        public int HotelId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<string> Amenities { get; set; }
+        public List<string> Photos { get; set; }
+
+    }
+}
